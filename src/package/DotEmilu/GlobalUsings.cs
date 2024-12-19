f3a5c981f457034a95ca7bbd23ec96adb23ee77e
Renzo Jared <renzojared_lm@hotmail.com>
2024-12-19 00:25:40 -0500

    feat: new implementation for presenter

--- src/package/DotEmilu/GlobalUsings.cs
+++ src/package/DotEmilu/GlobalUsings.cs
@@ -2,3 +2,5 @@
 
 global using FluentValidation.Results;
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Options;
---
+++ src/package/DotEmilu/Presenter.cs
@@ -0,0 +1,25 @@
+namespace DotEmilu;
+
+internal class Presenter(IOptions<ResultMessage> options) : IPresenter
+{
+    public IResult Success<TResponse>(in TResponse response)
+        => Results.Ok(response);
+
+    public IResult ValidationError(in IEnumerable<ValidationFailure> validationFailures)
+        => Results.ValidationProblem(
+            errors: validationFailures.GroupBy(f => f.PropertyName, f => f.ErrorMessage)
+                .ToDictionary(d => d.Key, d => d.ToArray()),
+            detail: options.Value.ValidationError.Detail,
+            instance: $"{nameof(ProblemDetails)}/{nameof(ValidationProblemDetails)}",
+            statusCode: StatusCodes.Status400BadRequest,
+            title: options.Value.ValidationError.Title,
+            type: options.Value.ValidationError.Type ?? "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1");
+
+    public IResult ServerError(in Exception exception)
+        => Results.Problem(
+            detail: options.Value.ServerError.Detail,
+            instance: $"{nameof(ProblemDetails)}/{exception.GetType().Name}",
+            statusCode: StatusCodes.Status500InternalServerError,
+            title: options.Value.ServerError.Title,
+            type: options.Value.ServerError.Type ?? "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1");
+}
