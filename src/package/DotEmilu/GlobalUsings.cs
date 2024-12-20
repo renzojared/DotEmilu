198331a8b3ff2c35fea7901a8370280ab0f13781
Renzo Jared <renzojared_lm@hotmail.com>
2024-12-20 09:58:47 -0500

    feat: new implementation for verifier

--- src/package/DotEmilu/GlobalUsings.cs
+++ src/package/DotEmilu/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Global using directives
 
+global using FluentValidation;
 global using FluentValidation.Results;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
---
+++ src/package/DotEmilu/Verifier.cs
@@ -0,0 +1,22 @@
+namespace DotEmilu;
+
+internal class Verifier<TRequest>(IEnumerable<IValidator<TRequest>> validators) : IVerifier<TRequest>
+{
+    public List<ValidationFailure> Errors { get; private set; } = [];
+
+    public async Task ValidateAsync(TRequest request, CancellationToken cancellationToken = default)
+    {
+        if (validators.Any())
+        {
+            var validationResults =
+                await Task.WhenAll(validators.Select(v => v.ValidateAsync(request, cancellationToken)));
+
+            Errors = validationResults
+                .Where(e => e.Errors.Count != 0)
+                .SelectMany(r => r.Errors)
+                .ToList();
+        }
+    }
+
+    public void AddErrors(in List<ValidationFailure> errors) => Errors.AddRange(errors);
+}
