dac7122f2ab6e8a1afffffd911b598783c1b6e34
Renzo Jared <renzojared_lm@hotmail.com>
2024-12-23 00:57:00 -0500

    feat: new standard handle

--- src/package/DotEmilu/IHandler.cs
+++ src/package/DotEmilu/IHandler.cs
@@ -4,3 +4,8 @@ public interface IHandler<in TRequest>
 {
     Task<IResult> HandleAsync(TRequest request, CancellationToken cancellationToken = default);
 }
+
+public interface IHandler
+{
+    Task HandleAsync(CancellationToken cancellationToken = default);
+}
