dc26519c0b3eb49ea100956335f58436e131bedd
Renzo Jared <renzojared_lm@hotmail.com>
2024-12-26 10:49:32 -0500

    feat: inherit own methods for error handling

--- src/package/DotEmilu/IVerifier.cs
+++ src/package/DotEmilu/IVerifier.cs
@@ -1,9 +1,6 @@
 namespace DotEmilu;
 
-public interface IVerifier<in TRequest>
+public interface IVerifier<in TRequest> : IVerifierError
 {
-    List<ValidationFailure> Errors { get; }
-    bool IsValid => Errors.Count == 0;
     Task ValidateAsync(TRequest request, CancellationToken cancellationToken = default);
-    void AddErrors(in List<ValidationFailure> errors);
 }
