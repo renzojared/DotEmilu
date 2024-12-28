f2c7557cb4f751c2e0dc44700c347a9052c7f452
Renzo Jared <renzojared_lm@hotmail.com>
2024-12-28 16:39:49 -0500

    feat: apply required section

--- src/package/DotEmilu/DiContainer.cs
+++ src/package/DotEmilu/DiContainer.cs
@@ -8,13 +8,7 @@ public static class DiContainer
     public static IServiceCollection AddDotEmilu(this IServiceCollection services, IConfiguration configuration)
         => services
             .Configure<ResultMessage>(message =>
-            {
-                var section = configuration.GetSection(ResultMessage.SectionKey);
-                if (section.Exists())
-                    section.Bind(message);
-                else
-                    ArgumentNullException.ThrowIfNull(section);
-            })
+                configuration.GetRequiredSection(ResultMessage.SectionKey).Bind(message))
             .AddScoped(typeof(IVerifier<>), typeof(Verifier<>))
             .AddScoped<IPresenter, Presenter>();
 }
