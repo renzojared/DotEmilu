0c60eee5f00a8f020e333a32169dceab2cb2fd45
Renzo Jared <renzojared_lm@hotmail.com>
2025-02-16 23:53:47 -0500

    refactor: change verifier methods at implementation

--- src/package/DotEmilu/Verifier.cs
+++ src/package/DotEmilu/Verifier.cs
@@ -1,8 +1,11 @@
 namespace DotEmilu;
 
 internal sealed class Verifier<TRequest>(IEnumerable<IValidator<TRequest>> validators) : IVerifier<TRequest>
+    where TRequest : IBaseRequest
 {
-    public List<ValidationFailure> Errors { get; private set; } = [];
+    private readonly List<ValidationFailure> _errors = [];
+    public IReadOnlyCollection<ValidationFailure> Errors => _errors;
+    public bool IsValid => Errors.Count == 0;
 
     public async Task ValidateAsync(TRequest request, CancellationToken cancellationToken)
     {
@@ -11,10 +14,18 @@ internal sealed class Verifier<TRequest>(IEnumerable<IValidator<TRequest>> valid
             var validationResults =
                 await Task.WhenAll(validators.Select(v => v.ValidateAsync(request, cancellationToken)));
 
-            Errors = validationResults
+            _errors.AddRange(validationResults
                 .Where(e => e.Errors.Count != 0)
-                .SelectMany(r => r.Errors)
-                .ToList();
+                .SelectMany(r => r.Errors));
         }
     }
+
+    public void AddErrors(in List<ValidationFailure> errors)
+        => _errors.AddRange(errors);
+
+    public void AddError(in ValidationFailure error)
+        => _errors.Add(error);
+
+    public void AddError(in string propertyName, in string errorMessage)
+        => _errors.Add(new ValidationFailure(propertyName, errorMessage));
 }
