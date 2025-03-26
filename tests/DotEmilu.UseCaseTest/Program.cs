0c5c2426c4affb23687676e3cb6b1bc38141ff48
Renzo Jared <renzojared_lm@hotmail.com>
2025-03-25 21:51:02 -0500

    reorder di

--- tests/DotEmilu.UseCaseTest/Program.cs
+++ tests/DotEmilu.UseCaseTest/Program.cs
@@ -1,13 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services
     .AddDotEmilu()
     .AddHandlers(Assembly.GetExecutingAssembly())
     .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
