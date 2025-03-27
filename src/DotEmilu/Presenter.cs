8dfce0685dba4c5c7ff32edf0d9605129761500c
Renzo Jared <renzojared_lm@hotmail.com>
2025-03-26 23:59:14 -0500

    refactor: change options snapshot to monitor for result message

--- src/DotEmilu/Presenter.cs
+++ src/DotEmilu/Presenter.cs
@@ -1,16 +1,16 @@
 namespace DotEmilu;
 
-internal sealed class Presenter(IOptionsSnapshot<ResultMessage> options, IHostEnvironment environment) : IPresenter
+internal sealed class Presenter(IOptionsMonitor<ResultMessage> options, IHostEnvironment environment) : IPresenter
 {
     public IResult ValidationError(in IEnumerable<ValidationFailure> validationFailures)
         => Results.ValidationProblem(
             errors: validationFailures.GroupBy(f => f.PropertyName, f => f.ErrorMessage)
                 .ToDictionary(d => d.Key, d => d.ToArray()),
-            detail: options.Value.ValidationError.Detail,
+            detail: options.CurrentValue.ValidationError.Detail,
             instance: $"{nameof(ProblemDetails)}/{nameof(ValidationProblemDetails)}",
             statusCode: StatusCodes.Status400BadRequest,
-            title: options.Value.ValidationError.Title,
-            type: options.Value.ValidationError.Type ?? "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1");
+            title: options.CurrentValue.ValidationError.Title,
+            type: options.CurrentValue.ValidationError.Type ?? "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1");
 
     public IResult ServerError(in Exception exception)
     {
@@ -19,11 +19,11 @@ internal sealed class Presenter(IOptionsSnapshot<ResultMessage> options, IHostEn
             : string.Empty;
 
         return Results.Problem(
-            detail: string.Join(" | ", new[] { options.Value.ServerError.Detail, additionalDetails }
+            detail: string.Join(" | ", new[] { options.CurrentValue.ServerError.Detail, additionalDetails }
                 .Where(s => !string.IsNullOrEmpty(s))),
             instance: $"{nameof(ProblemDetails)}/{exception.GetType().Name}",
             statusCode: StatusCodes.Status500InternalServerError,
-            title: options.Value.ServerError.Title,
-            type: options.Value.ServerError.Type ?? "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1");
+            title: options.CurrentValue.ServerError.Title,
+            type: options.CurrentValue.ServerError.Type ?? "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1");
     }
 }
