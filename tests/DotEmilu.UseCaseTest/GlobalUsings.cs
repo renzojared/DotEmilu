f179f1f9305af549fc231b27d0c0f346e7417c9f
Renzo Jared <renzojared_lm@hotmail.com>
2025-03-31 10:30:28 -0500

    refactor(test) : update test references

--- tests/DotEmilu.UseCaseTest/GlobalUsings.cs
+++ tests/DotEmilu.UseCaseTest/GlobalUsings.cs
@@ -2,6 +2,8 @@
 
 global using System.Reflection;
 global using DotEmilu;
+global using DotEmilu.Abstractions;
+global using DotEmilu.AspNetCore;
 global using DotEmilu.UseCaseTest.DTOs;
 global using DotEmilu.UseCaseTest.Endpoints;
 global using FluentValidation;
