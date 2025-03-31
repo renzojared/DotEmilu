225b41447b2d7b0acba2954d29dbfc7621ba54c9
Renzo Jared <renzojared_lm@hotmail.com>
2025-03-31 10:20:15 -0500

    refactor: change main project to only accept generic handler without external dependencies

--- src/DotEmilu/GlobalUsings.cs
+++ src/DotEmilu/GlobalUsings.cs
@@ -1,9 +1,5 @@
 // Global using directives
 
-global using System.ComponentModel.DataAnnotations;
+global using DotEmilu.Abstractions;
 global using FluentValidation;
 global using FluentValidation.Results;
-global using Microsoft.AspNetCore.Http;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.Extensions.Hosting;
-global using Microsoft.Extensions.Options;
