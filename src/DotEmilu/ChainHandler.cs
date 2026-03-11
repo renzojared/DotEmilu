8b0b8be97ffbb43d37dfe352c88f7305aaf3529d
Renzo Jared <renzojared_lm@hotmail.com>
2026-03-10 23:09:01 -0500

    refactor: make `Successor` property private for better encapsulation

--- src/DotEmilu/ChainHandler.cs
+++ src/DotEmilu/ChainHandler.cs
@@ -2,7 +2,7 @@ namespace DotEmilu;
 
 public abstract class ChainHandler<TChain>
 {
-    protected ChainHandler<TChain>? Successor;
+    protected ChainHandler<TChain>? Successor { get; private set; }
 
     public ChainHandler<TChain> SetSuccessor(ChainHandler<TChain> successor)
         => Successor = successor;
