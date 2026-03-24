af62b486107106a983259f43bb1f5e1fc22005f4
Renzo Jared <renzojared_lm@hotmail.com>
2026-03-24 09:11:45 -0500

    feat(samples): add ConfirmInvoice and SyncInvoices mappings to InvoiceEndpoints

--- samples/DotEmilu.Samples.FullApp/Features/InvoiceEndpoints.cs
+++ samples/DotEmilu.Samples.FullApp/Features/InvoiceEndpoints.cs
@@ -1,7 +1,9 @@
+using DotEmilu.Samples.FullApp.Features.ConfirmInvoice;
 using DotEmilu.Samples.FullApp.Features.CreateInvoice;
 using DotEmilu.Samples.FullApp.Features.DeleteInvoice;
 using DotEmilu.Samples.FullApp.Features.GetInvoiceById;
 using DotEmilu.Samples.FullApp.Features.GetInvoices;
+using DotEmilu.Samples.FullApp.Features.SyncInvoices;
 using DotEmilu.Samples.FullApp.Features.UpdateInvoice;
 
 namespace DotEmilu.Samples.FullApp.Features;
@@ -12,6 +14,8 @@ internal static class InvoiceEndpoints
     {
         app.MapGroup("/invoices").WithTags("Invoices")
             .MapCreateInvoice()
+            .MapConfirmInvoice()
+            .MapSyncInvoices()
             .MapGetInvoices()
             .MapGetInvoiceById()
             .MapUpdateInvoice()
