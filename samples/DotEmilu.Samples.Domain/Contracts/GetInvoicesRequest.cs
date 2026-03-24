f749205148fcd77c41f75a4b3e1594fb2e83f001
Renzo Jared <renzojared_lm@hotmail.com>
2026-03-23 19:14:38 -0500

    feat(samples): set default values for pagination in GetInvoicesRequest

--- samples/DotEmilu.Samples.Domain/Contracts/GetInvoicesRequest.cs
+++ samples/DotEmilu.Samples.Domain/Contracts/GetInvoicesRequest.cs
@@ -6,5 +6,5 @@ namespace DotEmilu.Samples.Domain.Contracts;
 /// Request to retrieve a paginated list of invoices.
 /// </summary>
 public record GetInvoicesRequest(
-    int PageNumber,
-    int PageSize) : IRequest<PaginatedList<Invoice>>;
+    int PageNumber = 1,
+    int PageSize = 10) : IRequest<PaginatedList<Invoice>>;
